eee1fdca9f4178e55c9b23d5d9743b66c1ef62cf
Amit Aryeh Levy <amit@amitlevy.com>
2020-10-15 16:58:00 -0400

    Add adam configuration

---
+++ machines/adam.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+let
+  hostname = "adam";
+  common = (import ./common.nix) { hostname = hostname; };
+  utils = import ../utils;
+in {
+
+  # Import common configurat for all machines (locale, SSHd, updates...)
+  imports = [ common ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  programs.mosh.enable = true;
+
+  users.users.alevy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "alevy";
+  };
+}
