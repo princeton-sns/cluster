be50d32fc1542cf2c6a3e4d586f7edc558a956e6
Jianan Lu <amberljn@berkeley.edu>
2020-10-18 13:34:43 -0400

    add jiananl to adam.nix

--- machines/adam.nix
+++ machines/adam.nix
@@ -35,4 +35,8 @@ in {
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = utils.githubSSHKeys "alevy";
   };
+  users.users.jiananl= {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "amberlu";
+  };
 }
