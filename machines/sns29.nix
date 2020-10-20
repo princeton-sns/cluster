63516ce26f02c1c5ccade989cb8983941cefbec0
Amit Aryeh Levy <amit@amitlevy.com>
2020-10-20 11:35:08 -0700

    Workstation for @alevy

---
+++ machines/sns29.nix
@@ -0,0 +1,30 @@
+# Configured as a workstation for @alevy, primarily for testing stuff on the
+# cluster. If this comment is still here after November 2020, you can take over
+# this machine with a PR.
+
+{ config, pkgs, ... }:
+
+let
+  hostname = "sns29";
+  common = (import ./common.nix) { hostname = hostname; };
+  utils = import ../utils;
+in {
+
+  # Import common configurat for all machines (locale, SSHd, updates...)
+  imports = [ common ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  ];
+
+  programs.mosh.enable = true;
+
+  users.mutableUsers = false;
+
+  users.users.alevy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "alevy";
+  };
+}
