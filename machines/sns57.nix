6a3c0a39d5cbd1d77eb2f6ee90fd2b5efc774161
theanoli <theanoli@users.noreply.github.com>
2020-11-02 18:16:24 -0500

    Update sns57.nix

--- machines/sns57.nix
+++ machines/sns57.nix
@@ -23,7 +23,7 @@ in {
 
   users.users.theano = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];	
+    extraGroups = [ "wheel", "kvm" ];	
     openssh.authorizedKeys.keys = utils.githubSSHKeys "theanoli";
   };
 }
