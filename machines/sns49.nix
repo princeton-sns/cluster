e7ac7109aca2cc0f8303c52b73571c7c51505c4e
linanqinqin <linanqinqin@princeton.edu>
2022-03-16 15:55:16 -0400

    Assign sns49 to Nan

---
+++ machines/sns49.nix
@@ -0,0 +1,28 @@
+# Configured as a workstation for @linanqinqin
+
+{ config, pkgs, ... }:
+
+let
+  hostname = "sns49";
+  common = (import ./common.nix) { hostname = hostname; };
+  utils = import ../utils;
+in {
+
+  # Import common configurat for all machines (locale, SSHd, updates...)
+  imports = [ common ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  ];
+
+  programs.mosh.enable = true;
+
+  users.mutableUsers = false;
+
+  users.users.alevy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "linanqinqin";
+  };
+}
