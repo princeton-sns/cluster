3c16e1ec4ead2c08e62ad8bf3b0da66dd21fb7c9
Amit Aryeh Levy <amit@amitlevy.com>
2022-09-02 20:56:00 -0400

    default logging

--- machines/matrix.nix
+++ machines/matrix.nix
@@ -98,33 +98,6 @@ in {
         service_url = "https://${fqdn}";
         displayname_attribute = "name";
       };
-      log_config = pkgs.writeText "log_config.yml" ''
-version: 1
-
-# In systemd's journal, loglevel is implicitly stored, so let's omit it
-# from the message text.
-formatters:
-    journal_fmt:
-        format: '%(name)s: [%(request)s] %(message)s'
-
-filters:
-    context:
-        (): synapse.util.logcontext.LoggingContextFilter
-        request: ""
-
-handlers:
-    journal:
-        class: systemd.journal.JournalHandler
-        formatter: journal_fmt
-        filters: [context]
-        SYSLOG_IDENTIFIER: synapse
-
-root:
-    level: WARN
-    handlers: [journal]
-
-disable_existing_loggers: False
-      '';
       listeners = [
         {
           port = 8448;
