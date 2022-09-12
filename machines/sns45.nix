c46e2ce5af4dd87f4afa84aa9290b0c304919414
Leon Schuermann <leon@is.currently.online>
2022-09-12 13:33:50 -0400

    sns45: assign to lschuermann
    Configures the host sns45 as a workstation for @lschuermann. Adds a
    selection of system packages. Furthermore, adds a root SSH key to
    allow for using the host as a Nix remote builder, while not having
    manage Nix derivation signing keys and trusting keys of the machines
    issuing remote builds.

--- machines/sns45.nix
+++ machines/sns45.nix
@@ -1,3 +1,5 @@
+# Configured as a workstation for @lschuermann
+
 { config, pkgs, ... }:
 
 let
@@ -12,9 +14,22 @@ in {
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    wget vim htop tmux nload iftop dnsutils gitAndTools.gitFull gnupg
+    gitAndTools.git-annex mtr bc zip unzip nmap nix-prefetch-git pdftk
+    imagemagick ghostscript rclone
   ];
 
   programs.mosh.enable = true;
 
   users.mutableUsers = false;
+
+  users.users.leons = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "lschuermann";
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOD4XspKe2E5BhBmx+GtRHdRR72+Q7wC7nFHbDj1VVzJ lschuermann/silicon/sns-nixbuild"
+  ];
 }
