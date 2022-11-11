82cec21596f030336821efa140411d89bf951c38
Gongqi Huang <46938348+cherrypiejam@users.noreply.github.com>
2022-11-11 16:57:52 -0500

    Setup for Fassten experiments

--- machines/sns59.nix
+++ machines/sns59.nix
@@ -13,6 +13,8 @@ in {
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
+    lkl lmdb python39Full e2fsprogs gnumake wget
+    vim tmux
   ];
 
   programs.mosh.enable = true;
@@ -37,9 +39,28 @@ in {
     openssh.authorizedKeys.keys = utils.githubSSHKeys "theanoli";
   };
 
-  users.users.leons = {
+  # For Faasten experiments
+  users.users.yuetan = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = utils.githubSSHKeys "lschuermann";
+    extraGroups = [ "wheel" "kvm" ];	
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "tan-yue";
+  };
+  
+  users.users.alevy = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "kvm" ];	
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "alevy";
+  };
+  
+  users.users.cherrypiejam = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "kvm" "docker" ];	
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "cherrypiejam";
+  };
+
+   users.users.npopescu = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "kvm" ];
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "nataliepopescu";
   };
 }
