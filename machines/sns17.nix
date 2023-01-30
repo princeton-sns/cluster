1e4614883b2cf4e7d7c96505c062726970f6fe3f
Amit Aryeh Levy <amit@amitlevy.com>
2023-01-30 09:44:01 -0500

    Use sns17 as new collective

--- machines/sns17.nix
+++ machines/sns17.nix
@@ -1,3 +1,4 @@
+# Collective is an OIT hosted VM that @alevy "owns"
 { config, pkgs, ... }:
 
 let
@@ -7,7 +8,7 @@ let
 in {
 
   # Import common configurat for all machines (locale, SSHd, updates...)
-  imports = [ common ];
+  imports = [ ../utils/deplorable.nix common ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -17,11 +18,73 @@ in {
 
   programs.mosh.enable = true;
 
-  virtualisation.docker.enable = true;
+  networking.firewall.allowedTCPPorts = [
+    # NGINX
+    80 443
+  ];
+
+  services.deplorable = {
+    enable = true;
+    config = {
+      repos = {
+        "sns" = {
+          repo = "princeton-sns/www";
+          reference = "refs/heads/master";
+          out = "sns.cs.princeton.edu";
+        };
+      };
+      repos = {
+        "systems" = {
+          repo = "PrincetonSystems/www";
+          reference = "refs/heads/master";
+          out = "princeton.systems";
+        };
+      };
+    };
+  };
+
+  services.nginx = {
+    enable = true;
+    virtualHosts."cos316.princeton.edu" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/lib/deplorable/cos316";
+      locations."/.deplorable" = {
+        proxyPass = "http://127.0.0.1:1337/cos316";
+      };
+    };
+    virtualHosts."os-seminar.princeton.systems" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/var/lib/deplorable/os-seminar";
+      locations."/.deplorable" = {
+        proxyPass = "http://127.0.0.1:1337/cos316";
+      };
+    };
+    virtualHosts."cos561.princeton.systems" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/home/rnetravali/public_html/cos561";
+    };
+    virtualHosts."ml-video-seminar.princeton.systems" = {
+      forceSSL = true;
+      enableACME = true;
+      root = "/home/rnetravali/public_html/cos561";
+    };
+  };
+
+  security.acme = {
+    defaults.email = "aalevy@princeton.edu";
+    acceptTerms = true;
+  };
 
   users.users.alevy = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = utils.githubSSHKeys "alevy";
+  };
+
+  users.users.rnetravali = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = utils.githubSSHKeys "ravinet";
   };
 }
