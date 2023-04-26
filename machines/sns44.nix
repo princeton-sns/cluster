b6436860f1b749e83d573be2525d26ae69e3c774
Amit Levy <amit@amitlevy.com>
2023-04-26 15:35:53 -0400

    Remove Kevin from sns44

--- machines/sns44.nix
+++ machines/sns44.nix
@@ -56,13 +56,6 @@ in {
     extraGroups = [ "wheel" "kvm" "docker" ];	
     openssh.authorizedKeys.keys = utils.githubSSHKeys "cherrypiejam";
   };
-  
-  ## Kevin Wang (@kw1122) working on snapfaas grader over summer '22
-  users.users.fierycandy = {
-    isNormalUser = true;
-    extraGroups = [ "kvm" ];
-    openssh.authorizedKeys.keys = utils.githubSSHKeys "kw1122";
-  };
 
   users.users.atli = {
     isNormalUser = true;
