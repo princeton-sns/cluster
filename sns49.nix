3bcfce7093f18ae7106afba27b9e1b898fbd3b0e
Leon Schuermann <leon@is.currently.online>
2023-05-29 12:33:40 -0400

    Add sns49 machine

---
+++ sns49.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ../sns-cluster
+  ];
+
+  networking = {
+    hostId = "fa526a52";
+    hostName = "sns49";
+
+    interfaces."enp1s0f0" = {
+      useDHCP = true;
+    };
+  };
+
+  sns-machine = {
+    enable = true;
+
+    family.beta = {
+      enable = true;
+
+      bootDisks = [ {
+        diskNode = "/dev/disk/by-id/wwn-0x50014ee2056e671c";
+        partUUID = "1854-B4E0";
+      } ];
+      swapPartUUIDs = [ "6f60a562-e50e-4751-9384-a739cf107b6c" ];
+    };
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+}
